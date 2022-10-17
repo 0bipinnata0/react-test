ca87fd9f6b6773440022283a273c10abf1151b78
tom <mail>
2022-10-17 22:17:05 +0800

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,45 @@
-
+import { faker } from "@faker-js/faker";
+import { useMemo, useState } from "react";
+import "./App.css";
 function App() {
+  const randomText = useMemo(() => faker.lorem.sentence(), []);
+  const [text, setText] = useState("");
+
   return (
-    <div className="App">
-      hello
+    <div className="container">
+      <div className="columns is-centered">
+        <div className="box column is-one-third">
+          <h1 className="title has-text-centered">Typing Challenge</h1>
+          <p className="has-text-centered">
+            {randomText.split("").map((char, index) => {
+              const enteredLetter = text[index];
+              const randomLetter = randomText[index];
+              let className = "pending";
+              if (enteredLetter) {
+                className =
+                  randomLetter === enteredLetter ? "correct" : "incorrect";
+              }
+              return (
+                <span key={index} className={className}>
+                  {char}
+                </span>
+              );
+            })}
+          </p>
+          <div className="field">
+            <input
+              className="input"
+              value={text}
+              onChange={(evt) => setText(evt.target.value)}
+            />
+          </div>
+          {text === randomText && (
+            <div className="box has-text-centered">
+              <h1>success</h1>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
